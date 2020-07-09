312d4dd81ac5ec8649f55c8c1765144ad7bee175
Pranav V K <Pranav.k28@wipro.com>
2020-07-10 01:11:36 +1000

    Exercise_01: Added podfile, added tableview for mainviewcontroller

--- ExerciseApp/Modules/Main/Controller/MainViewController.swift
+++ ExerciseApp/Modules/Main/Controller/MainViewController.swift
@@ -9,20 +9,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    var countryTableView = UITableView()
     
     let viewModel = CountryViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        configureTableView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         if connectedToNetwork() {
             viewModel.loadData(handler: { isLoaded in
                 if isLoaded {
                     weak var weakself = self
                     DispatchQueue.main.async {
-                        weakself?.showAlert(title: "Success", Message: "hurray")
-                        //TODO: Update UI Here
+                        weakself?.countryTableView.reloadData()
                     }
                 } else {
                     weak var weakself = self
@@ -36,9 +39,29 @@ class MainViewController: UIViewController {
         }
     }
     
+    func configureTableView() {
+        view.addSubview(countryTableView)
+        setTableViewDelegate()
+        countryTableView.rowHeight = 50
+    }
+    
+    func setTableViewDelegate() {
+        countryTableView.dataSource = self
+    }
+    
     func showAlert(title : String , Message : String) {
         let alert = UIAlertController(title: title, message: Message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.countryModel?.details.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
