da1b421160227a1ae0325d50db01d0c5470b434a
Pranav V K <Pranav.k28@wipro.com>
2020-07-15 16:07:46 +0530

    Exercise_01: Addressed review comments

---
+++ ExerciseApp/Launch/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ExerciseApp
+//
+//  Created by Pranav V K on 15/07/20.
+//  Copyright © 2020 Pranav V K. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    enum API {
+        static let countryDetails = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    }
+}
--- ExerciseApp/Modules/Main/ViewModel/CountryViewModel.swift
+++ ExerciseApp/Modules/Main/ViewModel/CountryViewModel.swift
@@ -19,7 +19,7 @@ final class CountryViewModel: NSObject {
      Method to make webservice call, parse json and update the country model with the result
      */
     func loadData(handler: @escaping(Bool) -> Void) {
-        makeWebServiceCall(from: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json", successHandler: { status, data in
+        makeWebServiceCall(from: Constants.API.countryDetails, successHandler: { status, data in
             do {
                 var modelData = try JSONDecoder().decode(CountryModel.self, from: data)
                 modelData.details = modelData.details.filter{ $0.title != nil || $0.description != nil || $0.imageUrl != nil}
